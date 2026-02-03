15300c4f5c1a6a25e91c4377a7947eae9c900433
harcastic <harsh.raj2274@gmail.com>
2026-02-03 19:53:07 +0530

    added mongodb-atlas

--- init/index.js
+++ init/index.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose  from "mongoose";
 import initData from "./data.js";
 import Listing  from '../models/listing.js';
 
-const mongo_URL = 'mongodb://127.0.0.1:27017/BookMyStay';
-
+// const mongo_URL = 'mongodb://127.0.0.1:27017/BookMyStay';
+const mongo_URL = process.env.MONGODB_URI;
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
 async function main() {
     await mongoose.connect(mongo_URL);
     console.log("connected to DB");
