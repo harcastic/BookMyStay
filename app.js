876275905cfd7a81ca4f2587984e132750813be9
harcastic <harsh.raj2274@gmail.com>
2026-02-21 01:56:34 +0530

    fixed bug and mereged both branches

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
-    // res.locals.author = req.user;
     next();
 })
 
